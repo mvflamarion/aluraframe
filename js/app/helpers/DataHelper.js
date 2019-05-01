cf07827abb9074921dbc0ae27e2265f6774da9c3
Marcos Flamarion <marcosflamarion@hotmail.com>
2019-05-01 13:06:31 -0300

    melhoria regex

--- js/app/helpers/DataHelper.js
+++ js/app/helpers/DataHelper.js
@@ -4,7 +4,7 @@ class DataHelper {
   }
 
   static textoParaData(texto) {
-    if (!/\d{4}-\d{2}-\d{2}/.test(texto))
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(texto))
       throw new Error("Deve estar no formato aaaa-mm-dd");
 
     return new Date(
